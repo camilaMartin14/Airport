2cbe74a31e9b4986aac75d75b1270dbcc36396df
camilaMartin14 <camilamartin1403@gmail.com>
2024-07-02 11:44:39 -0300

    subir

---
+++ Desktop/Ejercicios Spring Boot/TP-Integrador-1/src/main/java/com/example/TP/Integrador1/TpIntegrador1Application.java
@@ -0,0 +1,13 @@
+package com.example.TP.Integrador1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegrador1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegrador1Application.class, args);
+	}
+
+}
---
+++ Desktop/Ejercicios Spring Boot/TP-Integrador-1/src/test/java/com/example/TP/Integrador1/TpIntegrador1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP.Integrador1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrador1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
