d9a470df5ad5179aa38ee1fdf0d7a334b26ee4bb
camilaMartin14 <camilamartin1403@gmail.com>
2024-07-03 10:17:56 -0300

    subir base

---
+++ Desktop/TP Integrador 1 Spring Boot/TP-Integrador-1/src/main/java/com/tp_integrador_1/TP_Integrador_1/TpIntegrador1Application.java
@@ -0,0 +1,13 @@
+package com.tp_integrador_1.TP_Integrador_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegrador1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegrador1Application.class, args);
+	}
+
+}
---
+++ Desktop/TP Integrador 1 Spring Boot/TP-Integrador-1/src/test/java/com/tp_integrador_1/TP_Integrador_1/TpIntegrador1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_integrador_1.TP_Integrador_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrador1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
