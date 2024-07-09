d35ae8e0387574cf951a79e7b2fb1dffbb0427e0
camilaMartin14 <camilamartin1403@gmail.com>
2024-07-09 12:35:12 -0300

    subir api area del triangulo📐

--- Desktop/mini apis/area-del-triangulo/src/main/java/com/miniapis/area/del/triangulo/controller/TrianguloController.java
+++ Desktop/mini apis/area-del-triangulo/src/main/java/com/miniapis/area/del/triangulo/controller/TrianguloController.java
@@ -1,8 +1,15 @@
 package com.miniapis.area.del.triangulo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrianguloController {
-    
+    @GetMapping("/area-del-triangulo/base/{base}/altura/{altura}")
+    public String mostrarArea(@PathVariable double base,
+            @PathVariable double altura){
+    double area = (base * altura)/2;
+    return "El área del triángulo es de "+ area + "cm^2";
+    }
 }
